15fd23eeae357f30e77d66dc52542f22cb9503e0
Esteban-Lopez-aelc <163438249+Esteban-Lopez-aelc@users.noreply.github.com>
2024-04-04 17:29:15 -0500

    Update configuracion.php

--- base_de_datos/configuracion.php
+++ base_de_datos/configuracion.php
@@ -2,10 +2,10 @@
  #Archivo de configuracion de la base de datos
  
     define("PG_DB"  , "sigweb");
-	define("PG_HOST", "localhost");
+	define("PG_HOST", "bd1.cduyoesuqa1z.us-east-1.rds.amazonaws.com");
 	define("PG_USER", "postgres");
-	define("PG_PSWD", "123456");
-	define("PG_PORT", "5432");
+	define("PG_PSWD", "postgres");
+	define("PG_PORT", "postgres");
 	
 	$dbcon = pg_connect("dbname=".PG_DB." host=".PG_HOST." user=".PG_USER ." password=".PG_PSWD." port=".PG_PORT."");
 
